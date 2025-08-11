2733abb55e0119d8a83b1d15680c450d7c4cafd9
Taise_2525 <95949607+zebra2525Tai@users.noreply.github.com>
2025-08-11 10:38:01 +0900

    Add files via upload

---
+++ cat-painting/src/main/java/com/example/cat_painting/CatPaintingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cat_painting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatPaintingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatPaintingApplication.class, args);
+	}
+
+}
---
+++ cat-painting/src/test/java/com/example/cat_painting/CatPaintingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cat_painting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatPaintingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
